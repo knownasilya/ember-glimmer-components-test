963a067147a61240b95c5bef07ef047b4ddb616e
Ilya Radchenko <knownasilya@gmail.com>
2019-02-14 22:43:16 -0500

    fix: update imports and names

--- app/components/editor/component.js
+++ app/components/editor/component.js
@@ -1,17 +1,17 @@
 import Component from '@glimmer/component';
-import {EditorState} from 'prosemirror-state';
-import {EditorView} from 'prosemirror-view';
-import {Schema, DOMParser} from 'prosemirror-model';
-import {schema} from 'prosemirror-schema-basic';
-import {addListNodes} from 'prosemirror-schema-list';
-import {exampleSetup} from 'prosemirror-example-setup';
+import { EditorState } from 'prosemirror-state';
+import { EditorView } from 'prosemirror-view';
+import { Schema, DOMParser } from 'prosemirror-model';
+import { schema as basic } from 'prosemirror-schema-basic';
+import { addListNodes } from 'prosemirror-schema-list';
+import { exampleSetup } from 'prosemirror-example-setup';
 import { action } from '@ember-decorators/object';
 
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
-const mySchema = new Schema({
-  nodes: addListNodes(schema.spec.nodes, 'paragraph block*', 'block'),
-  marks: schema.spec.marks
+const schema = new Schema({
+  nodes: addListNodes(basic.spec.nodes, 'paragraph block*', 'block'),
+  marks: basic.spec.marks
 });
 
 export default class EditorComponent extends Component {
@@ -20,8 +20,8 @@ export default class EditorComponent extends Component {
     let contentEl = el.querySelector('.content');
     this.editorView = new EditorView(el, {
       state: EditorState.create({
-        doc: DOMParser.fromSchema(mySchema).parse(contentEl),
-        plugins: exampleSetup({schema: mySchema})
+        doc: DOMParser.fromSchema(schema).parse(contentEl),
+        plugins: exampleSetup({ schema })
       })
     });
   }
